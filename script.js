999b4ff7f9bf136f4b9ebe871b1fc67ec3f08af2
ms9 <ms_sm_11@outlook.com>
2022-11-14 01:02:03 +0200

    1st commit

---
+++ script.js
@@ -0,0 +1,34 @@
+alert("This website needs to access your location to display the correct information");
+
+navigator.geolocation.getCurrentPosition((position) => {
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    var url = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=4c5625a855d241482d21471074d82494`;
+
+    fetch(url).then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        if (data.weather[0].icon == "01d")
+            document.getElementById("icon").innerHTML = `<img src="https://cdn-icons-png.flaticon.com/512/869/869869.png"/>`;
+        else if (data.weather[0].icon == "01n")
+            document.getElementById("icon").innerHTML = `<img src="https://cdn-icons-png.flaticon.com/512/3106/3106764.png"/>`;
+        else
+            document.getElementById("icon").innerHTML = `<img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png"/>`;
+        var celsius = (parseFloat(data.main.temp) - 273.15).toFixed(2);
+        var fahrenheit = ((parseFloat(data.main.temp) * 9 / 5) - 459.67).toFixed(2);
+        document.getElementById("temp").innerHTML = celsius + "&deg;<sub>C</sub>";
+        document.getElementById("temp").addEventListener("click", function () {
+            if (document.getElementById("hidden").innerHTML == "0") {
+                document.getElementById("temp").innerHTML = fahrenheit + "&deg;<sub>F</sub>";
+                document.getElementById("hidden").innerHTML = "1";
+            } else {
+                document.getElementById("temp").innerHTML = celsius + "&deg;<sub>C</sub>";
+                document.getElementById("hidden").innerHTML = "0";
+            }
+        });
+        document.getElementById("dscp").innerHTML = data.weather[0].description;
+        document.getElementById("country").innerHTML = data.name + ", " + data.sys.country;
+    }).catch(function (error) {
+        alert(error);
+    });
+});
